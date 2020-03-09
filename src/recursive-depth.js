9d2f84afb4de044f409afd54afdc1808922b328a
Arkhipenka Andrei <europeoplebelarus@gmail.com>
2020-03-09 20:44:58 +0300

    feat:implemented recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,17 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
+    calculateDepth(arr) {
+    	let result = 0;
+    	if(Array.isArray(arr)){
+    		result++;
+    		for(let i = 0;i < arr.length; i++){
+    			if (Array.isArray(arr[i])) {
+    				result += this.calculateDepth(arr.flat());
+    				break;
+    			}
+    		}
+    		return result;
+    	}
+        
         // remove line with error and write your code here
     }
 };
