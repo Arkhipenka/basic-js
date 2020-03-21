df844bbb7e025214459ac57388c2878b53c58bcf
Arkhipenka Andrei <europeoplebelarus@gmail.com>
2020-03-21 17:23:38 +0300

    feat: simple-chain and carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,20 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity !== 'string') return false;
+
+  let sampleActivityFloat = parseFloat(sampleActivity);
+
+  if (isNaN(sampleActivityFloat)) return false;
+
+  if (sampleActivityFloat <= 0 || sampleActivityFloat > 15) return false;
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  
+  let sampleAge = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityFloat) / k);
+
+  return sampleAge;
+
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,24 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let newArr = [];
+
+    if (!Array.isArray(arr)) {
+        throw new Error('not an array');
+    } else if (arr.length < 1) {
+        return [];
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "--discard-next") {
+            i++;
+        } else if (arr[i] === "--discard-prev") {
+            newArr.pop()
+        } else if (arr[i] === "--double-next") {
+            newArr.push(arr[i + 1])
+        } else if (arr[i] === "--double-prev") {
+            newArr.push(arr[i - 1])
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr.filter(index => index !== undefined);;
 };
